cb54da4acba1cb09e6daa7616dd7b65e806bb938
Aditya Jha <adityajha.swe@gmail.com>
2024-05-20 16:19:11 -0500

    Updating State

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import Player from "./components/Players"
 
 function App() {
+  return <main>
+    <div id="game-container">
+      <ol id="players">
+        <Player name="Player 1" symbol="X"/>
+        <Player name="Player 2" symbol="O"/>
+      </ol>
+
+
+      GAME BOARD
+
+    </div>
+
+  </main>
   
 
-  return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+  
 }
 
 export default App
