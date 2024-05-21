7f5768665058b463401faeaa90ec17714b9f39d5
Aditya Jha <adityajha.swe@gmail.com>
2024-05-21 17:28:43 -0500

    Player Switch Function

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,14 +1,33 @@
+import { useState } from "react";
+
 const initialGameBoard=[
     [null, null,null],
     [null, null,null],
     [null, null,null]
 ];
 
-export default function Gameboard() {
+export default function Gameboard({onSelectSquare,activePlayerSymbol}) {
+    const [gameBoard, setGameBoard]=useState(initialGameBoard);
+
+    function handleSelectSquare(rowIndex, colIndex){
+        setGameBoard((prevGameBoard)=>{
+            const updateBoard=[...prevGameBoard.map(innerArray=>[...innerArray])];
+            updateBoard[rowIndex][colIndex]=activePlayerSymbol;
+            return updateBoard;
+        });
+
+        onSelectSquare();
+    }
+
+
+
     return <ol id ="game-board">
-        {initialGameBoard.map((row, rowIndex)=><li key={rowIndex}>
+        {gameBoard.map((row, rowIndex)=><li key={rowIndex}>
             <ol>
-                {row.map((playerSymbol, colIndex)=> <li key={colIndex}><button>{playerSymbol}</button></li>)}
+                {row.map((playerSymbol, colIndex)=> 
+                <li key={colIndex}>
+                    <button onClick={()=>handleSelectSquare(rowIndex,colIndex)}>{playerSymbol} </button>
+                </li>)}
             </ol>
         </li>)}
     </ol>
